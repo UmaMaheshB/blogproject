ad171b1868539b4058397d0536239554c715a915
uma bandi <uma.bandi@oracle.com>
2025-04-28 18:48:40 +0530

    25-04-2025: added models and migrations

---
+++ blog/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Category(models.Model):
+	name = models.CharField(max_length=100)
+	created_at = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return self.name
+
+class Post(models.Model):
+	title = models.CharField(max_length=200)
+	content = models.TextField()
+	image_url = models.URLField(blank=True, null=True)
+	author = models.ForeignKey(User, on_delete=models.CASCADE)
+	category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+
+	def __str__(self):
+		return self.title
