b884da8bac9be083375cdfd7fb1cd33f6e1d8382
Ayyappa <bandiayyappa.ab@gmail.com>
2025-05-07 20:59:43 +0530

    07-05-2025: added home url and fetching posts from Post Model

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from blog.models import Post
 
 # Create your views here.
+def home(request):
+	# posts = Post.objects.all()
+	posts = Post.objects.all().order_by("-id")
+	return render(request, "home.html", {"posts": posts})
+
+def news(request):
+	return HttpResponse("this is news related posts")
